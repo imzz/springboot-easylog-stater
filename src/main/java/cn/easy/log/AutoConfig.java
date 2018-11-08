7d61824ea72ba90a46712cdf2b39f0ca88a9fd5b
imzmn <572354908@qq.com>
2018-11-08 15:19:41 +0800

    结构调整

---
+++ src/main/java/cn/easy/log/AutoConfig.java
@@ -0,0 +1,10 @@
+package cn.easy.log;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AutoConfig {
+
+}
---
+++ src/test/java/cn/easy/log/MainTest.java
@@ -0,0 +1,5 @@
+package cn.easy.log;
+
+public class MainTest {
+
+}
